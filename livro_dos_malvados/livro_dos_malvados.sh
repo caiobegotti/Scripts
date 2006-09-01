252e0481b1b16576c03dd41c8d11016fb9db1243
Caio Begotti <caio1982@gmail.com>
2006-09-01 01:01:01 +0000

    mais detalhes de como fazer o script: ja baixa tudo e separa em grupos de 5 tiras por pagina

--- livro_dos_malvados/livro_dos_malvados.sh
+++ livro_dos_malvados/livro_dos_malvados.sh
@@ -1,20 +1,49 @@
-#!/bin/bash
+#!/bin/bash -xev
 #
 # Tue, 29 Aug 2006 20:16:44 -0300
 # caio begotti <caio@ueberalles.net>
 
-for num in $(seq 1 999)
+url=http://www.malvados.com.br
+
+rm -rf tirinhas/* paginas/* pdf/* temp/* grupos/*
+
+for num in $(seq 200 210)
 do
-	lynx --nolist -dump http://www.malvados.com.br/index${num}.html | sed '/^$/d;s/[[:blank:]]\+//;/\[tir/!d;s/[][]\+//g'
+	tirinha=$(lynx --nolist -dump ${url}/index${num}.html | sed '/^$/d;s/[[:blank:]]\+//;/\[tir/!d;s/[][]\+//g')
+	# arquivo=$(echo ${tirinha} | sed 's/tiramalvados/tirinha/;s/tirainicial/tirinha1/;s/tirinhar/tirinha/')
+
+	arquivo=$(date +%Y%m%d%H%M%S).raw
+	wget -N -q -c ${url}/${tirinha} -O tirinhas/${arquivo}
+	echo "... baixando imagem ${arquivo} que era ... ${tirinha}"
+done
+
+for tira in tirinhas/*; do file ${tira} | grep HTML && rm -rf ${tira}; done
+
+find tirinhas/ -iname "*.raw" > tirinhas/cruas.txt
+
+while read cur in
+do
+	convert -resize 591x188! ${cur} ${cur}.tmp
+	convert ${cur}.tmp -mattecolor white -frame 50x20+1+1 $(echo ${cur} | sed 's/...$/png/')
+done < tirinhas/cruas.txt
+
+ls -1 tirinhas/*.png > tirinhas/todas.txt
+
+while [ -s "tirinhas/todas.txt" ]
+do
+	sed '5q' tirinhas/todas.txt > grupos/$(echo $((RANDOM))).txt
+	sed -i '1,5d' tirinhas/todas.txt
 done
 
 exit 0
 
-# 1. baixa lista de tirinhas do site (nomes variam)
-# 2. baixa as tirinhas possiveis do site
-# 3. pega informacoes de todas as tirinhas
-# 4. deixa todas as tirinhas com o mesmo tamanho
-# 5. cria lista de tirinhas e separa em grupos de 5
+# tamanho das tirinhas: 591 x 188, formato .png
+
+# 1. baixa lista de tirinhas do site (nomes variam)	OK
+# 2. baixa as tirinhas possiveis do site		OK
+# 3. pega informacoes de todas as tirinhas		OK
+# 4. deixa todas as tirinhas com o mesmo tamanho	OK
+# 5. cria lista de tirinhas e separa em grupos de 5	OK
 # 6. monta paginas inteiras cruas com as 5 tiras
 # 7. cria moldura delas com labels de (tm) e numero
 # 8. insere cada pagina em um folha do PDF
@@ -34,6 +63,11 @@ montage -geometry +0+0 -background Black -fill white -label "- PÁGINA 7 -" final
 
 # pegar tamanho das imagens no formato do imagemagick
 identify tirinha300.gif | cut -d" " -f4
+identify 20060830223050.raw | cut -d" " -f4 | sed 's/x.*$//'
+identify 20060830223050.raw | cut -d" " -f4 | sed 's/^.*x//;s/+.*$//'
 
 # pegar a data de modificacao (criacao) do arquivo
 stat tirinha300.gif | sed '/[Mm]odify/!d;s/^.\{8\}//;s/ .*$//'
+
+# pega o numero da tirinha (do arquivo real)
+echo tirinha25.gif | sed 's/[[:alpha:]]\+//;s/....$//'
