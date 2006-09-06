c1c09cbcc34d8112f99899cb0e17278f524a67ff
Caio Begotti <caio1982@gmail.com>
2006-09-06 00:34:49 +0000

    tentando commitar uma versao final... vamos ver se essa vai (site ja ta no ar com um .pdf)

--- livro_dos_malvados/livro_dos_malvados.sh
+++ livro_dos_malvados/livro_dos_malvados.sh
@@ -1,49 +1,80 @@
-#!/bin/bash -ev
+#!/bin/bash -e
 #
 # Tue, 29 Aug 2006 20:16:44 -0300
 # caio begotti <caio@ueberalles.net>
 #
-# tamanhos de papel pra posicionamento das tirinhas
+# esse script meio porco serviu pra gerar um PDF das tirinhas dos malvados
+# usando somente os comandos lynx + wget + imagemagick + pdftk
+#
+# os sleeps entre comandos sao pra nao dar segfault (nao pergunte o porque)
+#
+# tamanhos de papel pra posicionamento das tirinhas:
 # http://upload.wikimedia.org/wikipedia/commons/b/b8/A_size_illustration.gif
-# livro do calvin e hobbes tem 830 de largura por 870 de altura
+#
+# livrao do calvin & hobbes tem 830 de largura por 870 de altura
+# tamanho padrao de cada tirinha: 591 x 188, formato .png
+#
+# pra botar borda branca na tira individual
+# convert testeY.png -mattecolor white -frame 50x20-0-0 testeX.png
+#
+# juntar varias tiras em uma imagem
+# convert 1.png 2.png * -append malvados.png
+#
+# botar bordinha soh pra dizer que tem
+# convert finalY.png -mattecolor white -frame 50x50+0+1 finalX.png
+#
+# adicionar numeracao na parte de cima
+# convert 1.png -fill white -box '#000000' -gravity North -annotate +0+0 '25' final.png
+#
+# botar copyright na pagina cheia de tirinhas
+# convert foo.png -gravity South -background White -splice 0x0 -draw "text 0,0 'Malvados é criação de André Dahmer'" bar.png
+#
+# gerar um PDF unico com multiplas paginas .pdf
+# pdftk *.pdf cat output livro.pdf
 
-url=http://www.malvados.com.br
+url="http://www.malvados.com.br"
 
-rm -rf tirinhas/* 
-rm -rf paginas/* 
-rm -rf pdf/* 
-rm -rf temp/* 
-rm -rf grupos/*
+clear
 
-for num in $(seq 1 999)
+rm -rf tirinhas
+rm -rf paginas
+rm -rf grupos
+rm -rf pdf
+
+mkdir {tirinhas,paginas,pdf,grupos}
+
+for num in $(seq  1 999)
 do
 	tirinha=$(lynx --nolist -dump ${url}/index${num}.html | sed '/^$/d;s/[[:blank:]]\+//;/\[tir/!d;s/[][]\+//g')
-	# arquivo=$(echo ${tirinha} | sed 's/tiramalvados/tirinha/;s/tirainicial/tirinha1/;s/tirinhar/tirinha/')
+	arquivo=$(date +%Y%m%d%H%M%S).1
 
-	arquivo=$(date +%Y%m%d%H%M%S).foo
 	wget -N -q -c ${url}/${tirinha} -O tirinhas/${arquivo}
 	echo "... baixando imagem ${arquivo} que era ... ${tirinha}"
 done
 
 for tira in tirinhas/*; do file ${tira} | grep HTML && rm -rf ${tira}; done
 
-find tirinhas/ -iname "*.foo" > tirinhas/cruas.txt
+find tirinhas/ -iname "*.1" > tirinhas/cruas.txt
 
 while read cur in
 do
-	convert -resize 591x188! ${cur} ${cur}.ok
+	convert -resize 591x188! ${cur} ${cur}.2
 	sleep 1
 
-	convert ${cur}.ok -mattecolor white -frame 50x20-0-0 ${cur}.png
+	convert ${cur}.2 -mattecolor white -frame 50x20-0-0 ${cur}.3
 	sleep 1
+
+	echo "... convertendo tirinha ${cur} e adicionando borda"
 
 done < tirinhas/cruas.txt
 
-ls -1 tirinhas/*.png > tirinhas/todas.txt
+ls -1 tirinhas/*.3 > tirinhas/todas.txt
+echo "... separando tiras em grupos de 3 por pagina"
 
 while [ -s "tirinhas/todas.txt" ]
 do
-	sed '3q' tirinhas/todas.txt > grupos/$(echo $((RANDOM))).txt
+	arquivo=$(date +%Y%m%d%H%M%S)
+	sed '3q' tirinhas/todas.txt > grupos/${arquivo}.txt
 	sed -i '1,3d' tirinhas/todas.txt
 done
 
@@ -51,56 +82,44 @@ for monte in grupos/*
 do
 	for tira in "$(cat ${monte})"
 	do
-		arquivo=$(echo $((RANDOM)))
+		echo "... criando pagina corrente ${tira} com copyright e salvando em PDF"
+		arquivo=$(date +%Y%m%d%H%M%S)
 
-		convert ${tira} -append paginas/${arquivo}.tudo
+		convert ${tira} -append paginas/${arquivo}.4
 		sleep 1
 
-		convert paginas/${arquivo}.tudo -mattecolor white -frame 0x20-0-0 paginas/${arquivo}.page
+		convert paginas/${arquivo}.4 -mattecolor white -frame 0x20-0-0 paginas/${arquivo}.5
 		sleep 1
 
-		convert paginas/${arquivo}.page -fill white -box '#000000' -gravity North -annotate +0+0 '   MALVADOS   ' paginas/${arquivo}.copy
+		convert paginas/${arquivo}.5 -fill white -box '#000000' -gravity North -annotate +0+0 '   MALVADOS   ' paginas/${arquivo}.6
 		sleep 1
 
-		convert paginas/${arquivo}.copy -gravity South -background White -splice 0x0 -draw "text 0,0 'Malvados é criação de André Dahmer. Todos os direitos reservados.'" paginas/${arquivo}.borda
+		convert paginas/${arquivo}.6 -gravity South -background White -splice 0x0 -draw "text 0,0 'Malvados é criação de André Dahmer. Todos os direitos reservados.'" paginas/${arquivo}.7
 		sleep 1
 
-		convert paginas/${arquivo}.borda -mattecolor white -frame 10x10+0+1 paginas/${arquivo}.pdf
+		convert paginas/${arquivo}.8 -mattecolor white -frame 10x10+0+1 paginas/${arquivo}.pdf
 		sleep 1
 
 	done
 done
 
-pdftk paginas/*.pdf cat output pdf/$(date +%Y%m%d).pdf
+livro_final=$(date +%Y%m%d).pdf
+paginas=$(ls -1 paginas/*.pdf)
+
+echo "... juntando todas as paginas em um PDF unico"
+pdftk ${paginas} cat output pdf/${livro_final}
+
+echo "... terminando de criar o arquivo livro_dos_malvados.pdf"
+convert malvados_livro_capa.png pdf/malvados_livro_capa.pdf
+convert malvados_livro_final.png pdf/malvados_livro_final.pdf
+
+pdftk	pdf/malvados_livro_capa.pdf	\
+	pdf/${livro_final}		\
+	pdf/malvados_livro_final.pdf	\
+	cat output pdf/livro_dos_malvados.pdf
+
+rm -rf tirinhas
+rm -rf paginas
+rm -rf grupos
 
 exit 0
-
-# tamanho das tirinhas: 591 x 188, formato .png
-
-# 1. baixa lista de tirinhas do site (nomes variam)	OK
-# 2. baixa as tirinhas possiveis do site		OK
-# 3. pega informacoes de todas as tirinhas		OK
-# 4. deixa todas as tirinhas com o mesmo tamanho	OK
-# 5. cria lista de tirinhas e separa em grupos de 5	OK
-# 6. monta paginas inteiras cruas com as 5 tiras	OK
-# 7. cria moldura delas com labels de (tm) e numero	OK
-# 8. insere cada pagina em um folha do PDF
-# 9. gerar o PDF final com capa, intro 
-
-# pra botar borda branca na tira individual
-convert input.gif -mattecolor white -frame 50x20-0-0 output.png
-
-# juntar varias tiras em uma imagem
-convert 1.png 2.png * -append malvados.jpg
-
-# bota bordinha "style", soh pra dizer que tem
-convert final.png -mattecolor white -frame 50x50+0+1 final.png
-
-# adiciona pagina na parte de cima
-convert 1.png -fill white -box '#000000' -gravity North -annotate +0+0 '   024   ' final.png
-
-# botar copyright na pagina cheia de tirinhas
-convert foo.png -gravity South -background White -splice 0x0 -draw "text 0,0 'Malvados é criação de André Dahmer. Todos os direitos reservados.'" final.png
-
-# gera um pdf a partir do template HTML com paginas
-pdftk *.pdf cat output caio.pdf
