4e68e0c190776ea75cb1310ec22d564d8b3a1b73
Caio Begotti <caio1982@gmail.com>
2007-03-19 14:30:44 +0000

    importing the script' skeleton into the repository

---
+++ bookmarks_merge.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -xev
+# a tiny script that merges tons of bookmarks.html from mozilla in a single one
+# caio begotti <caio@ueberalles.net> on Mon, 19 Mar 2007 11:26:59 -0300
+
+# input_list=$(sed '/A HREF/!d;s/^ \+//')
+# add_date=$(sed 's/^.* ADD_DATE="//;s/".*$//')
+# last_visit=$(sed 's/^.* LAST_VISIT="//;s/".*$//')
+# url=$(sed 's/^.*HREF="//;s/".*$//')
+# title=$(sed 's/<[^>]*>//g')
+
+exit 0
