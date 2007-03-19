f31c1d13183f5b3b5488be6cda4a567639af9cc7
Caio Begotti <caio1982@gmail.com>
2007-03-19 16:51:17 +0000

    added the main loop to start working on it... and I already knows how to get the useful data from the HTML files

--- bookmarks_merge.sh
+++ bookmarks_merge.sh
@@ -1,11 +1,31 @@
-#!/bin/bash -xev
+#!/bin/bash -e
 # a tiny script that merges tons of bookmarks.html from mozilla in a single one
 # caio begotti <caio@ueberalles.net> on Mon, 19 Mar 2007 11:26:59 -0300
 
-# input_list=$(sed '/A HREF/!d;s/^ \+//')
-# add_date=$(sed 's/^.* ADD_DATE="//;s/".*$//')
-# last_visit=$(sed 's/^.* LAST_VISIT="//;s/".*$//')
-# url=$(sed 's/^.*HREF="//;s/".*$//')
-# title=$(sed 's/<[^>]*>//g')
+input_file="$*"
+
+function func_print()
+{
+    echo -e "${1}"
+}
+
+temp=$(mktemp)
+cat $* > ${temp}
+
+input_list=$(sed '/A HREF/!d;s/^ \+//' ${temp})
+
+while read line
+do
+    add_date=$(echo ${line} | sed 's/^.* ADD_DATE="//;s/".*$//;s/[[:alpha:][:punct:]]//g')
+    last_visit=$(echo ${line} | sed 's/^.* LAST_VISIT="//;s/".*$//;s/[[:alpha:][:punct:]]//g')
+    url=$(echo ${line} | sed 's/^.*HREF="//;s/".*$//;s/<[^>]*>//g')
+    title=$(echo ${line} | sed 's/<[^>]*>//g')
+
+    func_print "adicionado...: ${add_date}"
+    func_print "visitado.....: ${last_visit}"
+    func_print "url..........: ${url}"
+    func_print "titulo.......: ${title}\n"
+
+done < ${temp}
 
 exit 0
