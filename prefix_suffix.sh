12bfcd622b64079e1ff5ca7b9ea653c2c29b3005
Caio Begotti <caio1982@gmail.com>
2007-08-31 13:42:42 +0000

    suporte a circumfixos simples adicionado

--- prefix_suffix.sh
+++ prefix_suffix.sh
@@ -13,20 +13,51 @@
 echo -e "Write down a few word bases separated by space (e.g. 'kiRa lOmE'):" && read base
 echo -e "Now gimme some prefixes to use, also separated by space (e.g. 'po ut'):" && read prefix
 echo -e "Then, separated by space, type a bunch of suffixes (e.g. 'om pRi'):" && read sufix
-echo -e "---------------------------------------------------------------------\n"
+echo -e "Ok then, now type some circumfixes with '-' as delimiter (e.g. 'ar-tuO'):" && read circumfix
+echo -e "To finish it, give me one or two infixes if you want (e.g. 'Eng Ri '):" && read infix
 
-for mid in ${base}
-do
-	echo "${mid}"
-	for left in ${prefix}
+function main()
+{
+	for circ in ${circumfix}
 	do
-		echo "${left}${mid}"
-		for right in ${sufix}
+		circbeg=$(echo ${circumfix} | cut -d- -f1)
+		circend=$(echo ${circumfix} | cut -d- -f2)
+
+		for mid in ${base}
 		do
-			echo "${mid}${right}"
-			echo "${left}${mid}${right}"
+			# lista base
+			echo "${mid}"
+
+			# lista base com circumfixo
+			echo "${circbeg}${mid}${circend}"
+
+			for left in ${prefix}
+			do
+				# lista prefixo na base
+				echo "${left}${mid}"
+
+				# lista prefixo na base com circumfixo
+				echo "${circbeg}${left}${mid}${circend}"
+
+				for right in ${sufix}
+				do
+					# lista base com sufixo
+					echo "${mid}${right}"
+
+					# lista base com sufixo e circumfixo
+					echo "${circbeg}${mid}${right}${circend}"
+	
+					# lista base com prefixo e sufixo
+					echo "${left}${mid}${right}"
+
+					# lista base com prefixo, sufixo e circumfixo
+					echo "${circbeg}${left}${mid}${right}${circend}"
+				done
+			done
 		done
 	done
-done | sort -u
+}
+
+main | sort -u
 
 exit 0
