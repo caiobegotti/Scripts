70751fa0696cc9fa55851c818012fe305fa4be99
Caio Begotti <caio1982@gmail.com>
2007-11-12 16:16:04 +0000

    ops, fixing the date stamp

--- csv2moneylog.sh
+++ csv2moneylog.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 temp=$(mktemp)
-sed 's/"/|/g;s/|,|/|/g' ${1} > ${temp}
+sed 's/"/|/g;s/|,|/|/g' ${*} > ${temp}
 
 while read line
 do
-	date=$(echo ${line} | cut -d'|' -f2)
+	fulldate=$(echo ${line} | cut -d'|' -f2)
+		month=$(echo ${fulldate} | cut -d/ -f1)
+		day=$(echo ${fulldate}   | cut -d/ -f2)
+		year=$(echo ${fulldate}  | cut -d/ -f3)
+		date=${year}-${month}-${day}
+
 	value=$(echo ${line}| cut -d'|' -f7)
 	desc=$(echo ${line} | cut -d'|' -f4)
 	echo -e "${date}\t${value}\t${desc}"
