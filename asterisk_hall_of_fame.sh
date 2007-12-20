a48db7576e6758313f639c7f2dda7cb2498cc107
Caio Begotti <caio1982@gmail.com>
2007-12-20 01:39:59 +0000

    uma ideia... parece que funciona, apesar do comando ser uma puta bosta suja :)

--- asterisk_hall_of_fame.sh
+++ asterisk_hall_of_fame.sh
@@ -4,6 +4,8 @@
 # svn blame outputs and rank developers by line written
 # caio begotti <caio@ueberalles>
 
+# cat ASTERISK_HALL_OF_FAME.txt | awk '{ print $2}' | sort -u | while read i; do grep $i ASTERISK_HALL_OF_FAME.txt | wc -l && echo $i; done | sed 'N;s/\n/ /'
+
 astdir=${1}
 rm -rf /tmp/${0}.*
 
