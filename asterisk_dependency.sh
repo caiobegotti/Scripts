4d15637af23ced933b2f851cd397b2642d8e690a
Caio Begotti <caio1982@gmail.com>
2008-01-11 21:51:58 +0000

    importing some random ideas about how i could make it

--- asterisk_dependency.sh
+++ asterisk_dependency.sh
@@ -1,3 +1,19 @@
 #!/bin/bash -xev
 
+find . -type f -iname '*.[hc]' | while read i; do cat $i | sed -e
+'/^#include/!d' -e 's/^.* //' -e 's/[><"*]//g' -e '/^[[:alnum:]]/!d';
+done | sort -u  > foo
+while read i; do find /usr/include /opt/local/include -iname "$i"; done < foo
+
+while read i; do find /usr/include | grep "/$i"; done < foo > foo1
+
+while read i; do LC_ALL=C rpm -qf $i; done < foo1 | sort -u | grep -v
+'not owned'
+
+while read i; do LC_ALL=C urpmf $i; done < foo1 | sed 's/:.*$//' | sort -u
+
+find . -type f -iname '*.[hc]' | while read i; do cat $i | sed -e
+'/^#include </!d' -e 's/^.* //' -e 's/[><"*]//g' -e
+'/^[[:alnum:]]/!d'; done | sort -u
+
 exit 0
