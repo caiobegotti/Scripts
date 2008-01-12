c7e9a540e758445b361f883a7bf0741f0d698de7
Caio Begotti <caio1982@gmail.com>
2008-01-12 00:15:34 +0000

    working version tested on my virtual debian etch

--- asterisk_dependency.sh
+++ asterisk_dependency.sh
@@ -1,19 +1,7 @@
-#!/bin/bash -xev
+#!/bin/bash
 
-find . -type f -iname '*.[hc]' | while read i; do cat $i | sed -e
-'/^#include/!d' -e 's/^.* //' -e 's/[><"*]//g' -e '/^[[:alnum:]]/!d';
-done | sort -u  > foo
-while read i; do find /usr/include /opt/local/include -iname "$i"; done < foo
+find . -type f -iname '*.[hc]' | while read i; do cat $i | sed -e '/^#include </!d' -e 's/^.* //' -e 's/[><"*]//g' -e '/^[[:alnum:]]/!d'; done | sort -u > foo
 
-while read i; do find /usr/include | grep "/$i"; done < foo > foo1
-
-while read i; do LC_ALL=C rpm -qf $i; done < foo1 | sort -u | grep -v
-'not owned'
-
-while read i; do LC_ALL=C urpmf $i; done < foo1 | sed 's/:.*$//' | sort -u
-
-find . -type f -iname '*.[hc]' | while read i; do cat $i | sed -e
-'/^#include </!d' -e 's/^.* //' -e 's/[><"*]//g' -e
-'/^[[:alnum:]]/!d'; done | sort -u
+while read i; do LC_ALL=C apt-file search "/usr/include/${i}" ; done < foo
 
 exit 0
