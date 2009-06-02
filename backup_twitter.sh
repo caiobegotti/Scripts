e2de90720e06bb3b172bc139f24802aa6fa0dfc7
Caio Begotti <caio1982@gmail.com>
2009-06-02 18:54:51 +0000

    script to fetch and backup all your twits on twitter.com

---
+++ backup_twitter.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+user=your_twitter_username
+pages=99
+
+for number in $(seq 1 ${pages}); do
+	sleep 3
+	lynx --source --nolist "http://twitter.com/${user}?page=${number}&twttr=true" | \
+	sed -e '/<ol /!d'                                                               \
+	    -e 's/<span /\n<span /g'                                                    \
+	    -e 's/<\/ol>//g'                                                            \
+	    -e 's/ \+<ol class=.*"timeline">//g'                                        \
+	    -e 's/<li /\n\n<li /g'                                                      \
+	    -e 's/<\/li>/\n<\/li>/g'                                                  | \
+	sed -e '/<span/s/^/\t/g'
+done
+
+exit 0
