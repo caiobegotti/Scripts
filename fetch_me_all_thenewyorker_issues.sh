3229e410bd4f2ab2da57824f6ef3eb45b4fdd3dd
Caio Begotti <caio1982@gmail.com>
2009-08-17 14:48:40 +0000

    starting engines

---
+++ fetch_me_all_thenewyorker_issues.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -xev
+#
+# caio begotti <caio1982@gmail.com> on Mon Aug 17 11:46:50 BRT 2009
+#
+# script created to automatically fetch all issues ever published
+# of the new yorker magazine (since 1925), username and password required.
+
+# get the issues list by year
+# http://archives.newyorker.com/global/content/GetArchive.aspx?pid=1012&type=IssuesForYear&Year=1999&var&rnd=0.7633019806586238
+
+# get a specific page, ready for printing
+# http://archives.newyorker.com/djvu/Conde%20Nast/New%20Yorker/2009_08_24/webimages/page0000001_print.jpg
+
+
+exit 0
