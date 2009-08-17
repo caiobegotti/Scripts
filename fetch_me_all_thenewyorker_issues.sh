c1f60ff4846971f73c41f2c1d56226c337d586e8
Caio Begotti <caio1982@gmail.com>
2009-08-17 18:06:05 +0000

    just a test... will it work?

--- fetch_me_all_thenewyorker_issues.sh
+++ fetch_me_all_thenewyorker_issues.sh
@@ -25,7 +25,7 @@ function do_login()
 function get_issues_by_year() {
 	for year in $(seq 2009 $(date +%Y)); do
 		url=http://archives.newyorker.com/global/content/GetArchive.aspx\?pid=1012\&type=IssuesForYear\&Year=${year}
-		lynx -useragent 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_5_7; en-us) AppleWebKit/530.19.2 (KHTML, like Gecko) Version/4.0.2 Safari/530.19' -dump --nolist ${url} | cut -d: -f3 | sed '/_/!d;s/"//g;s/,.*$//'
+		lynx -dump --nolist ${url} | cut -d: -f3 | sed '/_/!d;s/"//g;s/,.*$//'
 	done | sort -u
 }
 
@@ -33,7 +33,7 @@ function get_pages_from_issue() {
 	for issue in $(get_issues_by_year); do
 		test -d ${issue} || mkdir -p ${issue}
 		for page in $(seq -w 001 300); do
-			wget --load-cookies ${cookies} --user-agent 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_5_7; en-us) AppleWebKit/530.19.2 (KHTML, like Gecko) Version/4.0.2 Safari/530.19' http://archives.newyorker.com/djvu/Conde%20Nast/New%20Yorker/${issue}/webimages/page0000${page}_print.jpg -O ${issue}/page_${page}.jpg
+			wget --load-cookies ${cookies} "http://archives.newyorker.com/djvu/Conde Nast/New Yorker/${issue}/webimages/page0000${page}_print.jpg" -O ${issue}/page_${page}.jpg
 		done
 
 		find ${issue} -size 0 -exec rm -rf {} \;
