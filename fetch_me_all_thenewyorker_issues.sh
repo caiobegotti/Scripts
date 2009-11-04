c423a62386a8be81240457a59a53ab606c1ad217
Caio Begotti <caio1982@gmail.com>
2009-11-04 00:22:41 +0000

    more cleanup for nicer readability and usability

--- fetch_me_all_thenewyorker_issues.sh
+++ fetch_me_all_thenewyorker_issues.sh
@@ -2,15 +2,10 @@
 #
 # caio begotti <caio1982@gmail.com> on Mon Aug 17 11:46:50 BRT 2009
 #
-# script created to automatically fetch all issues ever published
-# of the new yorker magazine (since 1925), username and password required.
+# script created to automatically fetch all issues ever published of
+# the new yorker magazine (since 1925), you will need cookies (and milk)
 
-# get the issues list by year
-# http://archives.newyorker.com/global/content/GetArchive.aspx?pid=1012&type=IssuesForYear&Year=1999&var&rnd=0.7633019806586238
-# lynx -dump --nolist 'http://archives.newyorker.com/global/content/GetArchive.aspx?pid=1012&type=IssuesForYear&Year=1925' | cut -d: -f3 | sed '/_/!d;s/"//g;s/,.*$//'
-
-# get a specific page, ready for printing
-# http://archives.newyorker.com/djvu/Conde%20Nast/New%20Yorker/2009_08_24/webimages/page0000001_print.jpg
+test -n ${1} && echo "${0} [year(s)]"
 
 year=${1}
 
