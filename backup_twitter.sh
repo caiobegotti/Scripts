5ccfb09f9c1636bf97b21c46a33e4ce5231ef55b
Caio Begotti <caio1982@gmail.com>
2010-01-20 18:35:38 +0000

    fix the html parsing

--- backup_twitter.sh
+++ backup_twitter.sh
@@ -1,18 +1,12 @@
-#!/bin/bash -xv
+#!/bin/bash
 
 user=${1}
 pages=${2}
 
 for number in $(seq 1 ${pages}); do
 	sleep 3
-	lynx --source --nolist "http://twitter.com/${user}?page=${number}&twttr=true" | \
-	sed -e '/<ol /!d'                                                               \
-	    -e 's/<span /\n<span /g'                                                    \
-	    -e 's/<\/ol>//g'                                                            \
-	    -e 's/ \+<ol class=.*"timeline">//g'                                        \
-	    -e 's/<li /\n\n<li /g'                                                      \
-	    -e 's/<\/li>/\n<\/li>/g'                                                  | \
-	sed -e '/<span/s/^/\t/g;/^$/d'
+	lynx --source --nolist "http://twitter.com/${user}?page=${number}&twttr=true" | sed '/span class="entry-content/,/<span>from/!d;/meta entry-meta/d;s/            /<\/p>\n<p>\n  /g' | sed '1d'
+	echo "</p>"
 done
 
 exit 0
