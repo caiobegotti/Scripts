f38c404d6fffbbaafebc1b83c49e3862a7a8ed76
Caio Begotti <caio1982@gmail.com>
2010-06-21 14:46:01 +0000

    thereifixit

--- backup_twitter.sh
+++ backup_twitter.sh
@@ -5,8 +5,8 @@ pages=${2}
 
 for number in $(seq 1 ${pages}); do
 	sleep 3
-	lynx --source --nolist "http://twitter.com/${user}?page=${number}&twttr=true" | sed '/span class="entry-content/,/<span>from/!d;/meta entry-meta/d;s/            /<\/p>\n<p>\n  /g' | sed '1d'
-	echo "</p>"
+	lynx --source --nolist "http://twitter.com/${user}?page=${number}&twttr=true" | sed '/^.*status-body.*/,/^.*meta-data clearfix.*/!d;/ul class/d;s/^.*status-body.*/<\/p>\n<p>/g' | sed 1d
+	echo '</p>'
 done
 
 exit 0
