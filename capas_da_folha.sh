1bda9bafd8e45afe9751b35ac1213df62c8b6114
Caio Begotti <caio1982@gmail.com>
2011-07-25 16:14:59 +0000

    cagada, greater equal e nao greater than, senao janeiro nunca sera pego

--- capas_da_folha.sh
+++ capas_da_folha.sh
@@ -6,6 +6,6 @@
 #
 # TODO: eh capa pra caralho, seria bom fazer uma galeria decente com o llgal
 
-ano=$(date +%Y); while [ $ano -ge 2000 ]; do mes=12; while [ $mes -gt 01 ]; do dia=31; while [ $dia -ge 01 ]; do test $mes -ge 10 && mesx=$mes || mesx=0${mes}; test $dia -ge 10 && diax=$dia || diax=0${dia} ; echo ${diax}${mesx}${ano} && let dia-- ; done && let mes-- ; done && let ano-- ; done | tail -r | while read i; do wget -c --timeout=60 --tries=6 http://www1.folha.uol.com.br/fsp/images/cp${i}.jpg; done
+ano=$(date +%Y); while [ $ano -ge 2000 ]; do mes=12; while [ $mes -ge 01 ]; do dia=31; while [ $dia -ge 01 ]; do test $mes -ge 10 && mesx=$mes || mesx=0${mes}; test $dia -ge 10 && diax=$dia || diax=0${dia} ; echo ${diax}${mesx}${ano} && let dia-- ; done && let mes-- ; done && let ano-- ; done | tail -r | while read i; do wget -c --timeout=60 --tries=6 http://www1.folha.uol.com.br/fsp/images/cp${i}.jpg; done
 
 exit 0
