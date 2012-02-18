96b7f23fede2748a6a4dee8c1ebc767c8bb1d5b3
Caio Begotti <caio1982@gmail.com>
2012-02-18 18:48:29 -0200

    avoid breaking long lines

--- getlat.sh
+++ getlat.sh
@@ -3,7 +3,7 @@
 # caio1982@gmail.com (originally published at https://gist.github.com/935298)
 # get latin definitions from wiktionary on your shell, accepts compounds and latin affixes: getlat 'word'
 
-function getlat() { lynx -source --nolist http://en.wiktionary.org/wiki/${@//[[:space:]]/_} | sed '/<h2>.*Latin/,/<h2>/!d;/<h2>/d' | lynx -assume-charset=UTF-8 -stdin -dump --nolist | sed 's/\[edit\] //;s/_//g;/Retrieved/,//d'; }
+function getlat() { lynx -source --nolist http://en.wiktionary.org/wiki/${@//[[:space:]]/_} | sed '/<h2>.*Latin/,/<h2>/!d;/<h2>/d' | lynx -width=160 -assume-charset=UTF-8 -stdin -dump --nolist | sed 's/\[edit\] //;s/_//g;/Retrieved/,//d'; }
 
 getlat "${1}"
 
