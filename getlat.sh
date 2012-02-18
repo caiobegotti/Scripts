e9575d75929811450aa9ec06fef36341fe40e49b
Caio Begotti <caio1982@gmail.com>
2012-02-17 23:34:22 -0200

    get latin definitions from wiktionary on your shell

---
+++ getlat.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -xv
+#
+# caio1982@gmail.com (originally published at https://gist.github.com/935298)
+# get latin definitions from wiktionary on your shell, accepts compounds and latin affixes: getlat 'word'
+
+function getlat() { lynx -source --nolist http://en.wiktionary.org/wiki/${@//[[:space:]]/_} | sed '/<h2>.*Latin/,/<h2>/!d;/<h2>/d' | lynx -assume-charset=UTF-8 -stdin -dump --nolist | sed 's/\[edit\] //;s/_//g;/Retrieved/,//d'; }
+
+getlat ${1}
+
+exit 0
