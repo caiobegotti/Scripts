db25dd2ac23d35cd4e9f608585c702d0d48c749e
Caio Begotti <caio1982@gmail.com>
2012-02-19 17:23:40 -0200

    fuck it, i will use popen to lynx and that is all

--- getlat.py
+++ getlat.py
@@ -10,6 +10,9 @@ from sys import exit
 from lxml.html import tostring
 from lxml import etree
 
+from os import popen
+import tempfile
+
 if not len(argv) == 2:
     exit('Usage: ' + argv[0] + " 'your term'")
 
@@ -19,9 +22,25 @@ parser = etree.HTMLParser()
 try:
     tree = etree.parse('http://en.wiktionary.org/wiki/' + term, parser)
     elements = tree.xpath('/html/body/div[3]/div[3]/div[4]')
+    html = tostring(elements[0], encoding='utf-8', method='html', pretty_print=True).replace('[edit] ','')
+    try:
+        # now, this is quite embarassing... i understand how ugly it is to use popen to lynx here
+        # the problem is that most scrapping solutions such as scrapy, beautifulsoup and lxml
+        # actually do a good job with serialized content and line-by-line scrapping and such
+        # but they can't simply render a given block of a page's indented code... lynx does
+        # i could also have manually handled this, but if they won't keep indentation and
+        # white spaces i guess there's no point in doing it myself
+
+        file = tempfile.NamedTemporaryFile()
+        filename = file.name
+        file.write(html)
+        file.seek(0)
+        of = popen('cat %s | lynx -assume-charset=UTF-8 -dump -nolist -width=200 -stdin' % filename)
+        buffer = of.read().replace('[edit] ','')
+        of.close()
+        file.flush()
+        print buffer
+    except:
+        exit('Could not render the page from wiktionary')
 except:
     exit('Term not found or wiktionary unavailable now')
-    
-for div in elements:
-    res = tostring(div, encoding='utf-8', method='text', pretty_print=True)
-    print res.replace('[edit] ','').replace('\n\n','\n')
