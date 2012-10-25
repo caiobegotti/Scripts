08e9225a8f81fa1d570c7d9910235a3b4ba252d8
Claudio Matsuoka <cmatsuoka@gmail.com>
2012-10-25 17:30:31 -0200

    Structure source using functions
    Define functions for the main blocks of code: server connection,
    message filtering and image retrieval.
    
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

--- LostPhotosFound.py
+++ LostPhotosFound.py
@@ -29,48 +29,60 @@ USERNAME = 'username@gmail.com'
 PASSWORD = 'password'
 SSL = True
 
-try:
-    server = IMAPClient(HOST, use_uid=True, ssl=SSL)
-except:
-    raise Exception('Could not successfully connect to the IMAP host')
+def get_server(host, username, password):
+    try:
+        server = IMAPClient(host, use_uid=True, ssl=SSL)
+    except:
+        raise Exception('Could not successfully connect to the IMAP host')
 
-server.login(USERNAME, PASSWORD)
-server.select_folder('[Gmail]/All Mail')
+    server.login(username, password)
+    server.select_folder('[Gmail]/All Mail')
 
-# that's why we only support gmail
-# for other mail services we'd have to translate the custom
-# search to actual IMAP queries, thus no X-GM-RAW cookie to us
-criteria = 'X-GM-RAW "has:attachment filename:(jpg OR jpeg OR gif OR png OR tiff OR tif OR ico OR xbm OR bmp)"'
-messages = server.search([criteria])
+    return server
 
-# stats
-print 'LOG: %d messages matched the search criteria %s' % (len(messages), criteria)
 
-for m in messages:
-    data = server.fetch([m], ['RFC822'])
-    for d in data:
-        # SEQ is also available
-        mail = email.message_from_string(data[d]['RFC822'])
-        if mail.get_content_maintype() != 'multipart':
-            continue
-        print "["+mail["From"]+"]: " + mail["Subject"]
-        for part in mail.walk():
-            if part.get_content_maintype() == 'multipart':
+def get_messages(server):
+    # that's why we only support gmail
+    # for other mail services we'd have to translate the custom
+    # search to actual IMAP queries, thus no X-GM-RAW cookie to us
+    criteria = 'X-GM-RAW "has:attachment filename:(jpg OR jpeg OR gif OR png OR tiff OR tif OR ico OR xbm OR bmp)"'
+    messages = server.search([criteria])
+
+    # stats
+    print 'LOG: %d messages matched the search criteria %s' % (len(messages), criteria)
+
+    return messages
+
+def filter_content(messages):
+    for m in messages:
+        data = server.fetch([m], ['RFC822'])
+        for d in data:
+            # SEQ is also available
+            mail = email.message_from_string(data[d]['RFC822'])
+            if mail.get_content_maintype() != 'multipart':
                 continue
-            if part.get('Content-Disposition') is None:
-                continue
-            filename = part.get_filename()
-            seq = 1
-            if not filename:
-                filename = 'attachment-%03d%s' % (seq, 'bin')
-                seq += 1
-            if not os.path.isdir(USERNAME):
-                os.mkdir(USERNAME)
-            saved = os.path.join(USERNAME, filename)
-            if not os.path.isfile(saved):
-                f = open(saved, 'wb')
-                f.write(part.get_payload(decode=True))
-                f.close()
+            print "["+mail["From"]+"]: " + mail["Subject"]
+            for part in mail.walk():
+                if part.get_content_maintype() == 'multipart':
+                    continue
+                if part.get('Content-Disposition') is None:
+                    continue
+                filename = part.get_filename()
+                seq = 1
+                if not filename:
+                    filename = 'attachment-%03d%s' % (seq, 'bin')
+                    seq += 1
+                if not os.path.isdir(USERNAME):
+                    os.mkdir(USERNAME)
+                saved = os.path.join(USERNAME, filename)
+                if not os.path.isfile(saved):
+                    f = open(saved, 'wb')
+                    f.write(part.get_payload(decode=True))
+                    f.close()
+    
 
+server = get_server(HOST, USERNAME, PASSWORD)
+messages = get_messages(server)
+filter_content(messages)
 server.close_folder()
 server.logout()
