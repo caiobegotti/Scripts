2c2a6e8288cd899fdff59089bc20e92cb96667b8
Caio Begotti <caio1982@gmail.com>
2014-02-11 17:47:38 -0200

    so i can use chromes cookies exported to wget

--- movies_lengths_over_time.sh
+++ movies_lengths_over_time.sh
@@ -1,15 +1,15 @@
 #!/bin/bash -xv
 
-base_url='http://www.imdb.com/search/title?at=0&sort=moviemeter,asc&start=99951&title_type=feature&tok=f686&year=1900,2015'
+base_url='http://www.imdb.com/search/title?at=0&count=100&sort=moviemeter,asc&start=99951&title_type=feature&tok=17bd&year=1900,2015'
 
-wget --save-cookies cookies.txt --keep-session-cookies ${base_url} -O base_url.html
+wget -x --load-cookies cookies.txt ${base_url} -O base_url.html
 
 url=$(sed '/Next/!d;s/^.*="//g;s/".*$//g' base_url.html | head -1)
 
 while true; do
 	count=$(echo ${url} | sed 's/^.*start=\([0-9]\{1,\}\)\&.*/\1/g')
 	if [ ! -e ${count}.html ]; then
-		wget --load-cookies cookies.txt --keep-session-cookies "http://www.imdb.com${url}" -O ${count}.html || exit 1
+		wget -x --load-cookies cookies.txt "http://www.imdb.com${url}" -O ${count}.html || exit 1
 	fi
         url=$(sed '/Next/!d;s/^.*="//g;s/".*$//g' ${count}.html | head -1)
 done
