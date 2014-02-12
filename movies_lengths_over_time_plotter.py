7638f035f08b47921aacc051284f23fe39759cbb
Caio Begotti <caio1982@gmail.com>
2014-02-11 22:59:57 -0200

    plot also the average length of movies per year

--- movies_lengths_over_time_plotter.py
+++ movies_lengths_over_time_plotter.py
@@ -4,23 +4,41 @@
 # this file is under public domain
 # caio begotti <caio1982@gmail.com>
 
-import pandas
-
 from matplotlib import pyplot
 
+import numpy
+import pandas
+
 columns = ['titles', 'years', 'runtimes', 'links']
-data = pandas.read_csv('imdb.txt', sep='\t', names=columns)
+dtype = {'titles': numpy.object, 'years': numpy.int64, 'runtimes': numpy.int64, 'links': numpy.object}
+data = pandas.read_csv('imdb.txt', sep='\t', names=columns, dtype=dtype)
 years = list(data.years)
 runtimes = list(data.runtimes)
 
 fig = pyplot.figure(figsize=(50, 10), dpi=100)
-g = fig.add_subplot(121)
-g.scatter(years, runtimes, marker='x', s=50, linewidths=0.25, alpha=0.25, cmap=pyplot.cm.coolwarm)
+
+# all movies plotted -------------------------
+m = fig.add_subplot(121)
+m.scatter(years, runtimes, marker='x', s=50, linewidths=0.25, alpha=0.25)
 
 # we have only a few releases over 300 minutes
 # of length, they would pollute the plot
-g.set_xlim([1900, 2015])
-g.set_ylim([0, 300])
+m.set_xlim([1900, 2015])
+m.set_ylim([0, 300])
+
+# averages per year --------------------------
+releases = {}
+for t in sorted(set(years)):
+    thisyear = []
+    for y, r in zip(years, runtimes):
+        if y == t:
+            thisyear.append(r)
+    total = len(thisyear)
+    releases[t] = sum(thisyear) / total
+
+a = fig.add_subplot(121)
+a.plot(releases.keys(), releases.values(), color='w', linestyle='-', linewidth=5)
+a.plot(releases.keys(), releases.values(), color='r', linestyle='-', linewidth=3)
 
 # labels
 pyplot.title('Movies runtimes over the years (~153k films)')
