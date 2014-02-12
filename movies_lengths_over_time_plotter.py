8ef55434c53e63b8dd7af055ecc3da45e304022e
Caio Begotti <caio1982@gmail.com>
2014-02-12 21:59:18 -0200

    i will need to figure out a way to make this code cleaner and smarter, but anyway i can plot just fine now the stacked bars on plot.ly

--- movies_lengths_over_time_plotter.py
+++ movies_lengths_over_time_plotter.py
@@ -41,17 +41,40 @@ fig = pyplot.figure(figsize=(25, 25), dpi=100)
 #a.plot(averages.keys(), averages.values(), color='r', linestyle='-', linewidth=3)
 
 # total number of releases per year ----------
-releases = {}
-for t in sorted(set(years)):
-    thisyear = []
-    for y, r in zip(years, runtimes):
-        if y == t:
-            thisyear.append(r)
-    releases[t] = len(thisyear)
+#releases = {}
+#for t in sorted(set(years)):
+#    thisyear = []
+#    for y, r in zip(years, runtimes):
+#        if y == t:
+#            thisyear.append(r)
+#    releases[t] = len(thisyear)
+#
+#r = fig.add_subplot(211)
+#r.set_xlim([1900, 2014])
+#r.plot(releases.keys(), releases.values(), color='g', linestyle='-', linewidth=5)
 
-r = fig.add_subplot(211)
-r.set_xlim([1900, 2014])
-r.plot(releases.keys(), releases.values(), color='g', linestyle='-', linewidth=5)
+# stacked bars of common lengths -------------
+#stacked = {}
+#for t in sorted(set(years)):
+#    t1, t2, t3, t4 = 0, 0, 0, 0
+#    for y, r in zip(years, runtimes):
+#        if r <= 60 and y == t:
+#            t1 += 1
+#        if r > 60 and r <= 120 and y ==t:
+#            t2 += 1
+#        if r > 120 and r <= 180 and y == t:
+#            t3 += 1
+#        if r > 180 and y == t:
+#            t4 += 1
+#    stacked[t] = (t1, t2, t3, t4)
+#
+# i can simply plot.ly for the stacked bars instead
+#with open('plotly.csv', 'a') as f:
+#    f.seek(0)
+#    for y in sorted(set(years)):
+#        row = '%s; %s; %s; %s; %s\n' % (y, stacked[y][0], stacked[y][1], stacked[y][2], stacked[y][3])
+#        f.write(row)
+#    f.close()    
 
 # labels
 pyplot.title('Number of movies releases per year (~153k films)')
@@ -60,7 +83,7 @@ pyplot.ylabel('Total')
 pyplot.grid(True)
 
 # increase x time resolution
-yearsticks = sorted(set([int(x) for x in years if x % 10 == 0]))
-pyplot.xticks(yearsticks)
+#yearsticks = sorted(set([int(x) for x in years if x % 10 == 0]))
+#pyplot.xticks(yearsticks)
 
 pyplot.savefig('imdb_releases.png', bbox_inches='tight', dpi=100)
