6d8418edc0910b7918b45a1e71fa7c701156bbb4
Caio Begotti <caio1982@gmail.com>
2014-02-15 23:15:03 -0300

    works fine in a loop, so i can just leave it running for like, forever, until it gets the job done

--- movies_lengths_over_time_downloader.sh
+++ movies_lengths_over_time_downloader.sh
@@ -1,17 +1,49 @@
-#!/bin/bash -xv
+#!/bin/bash -xev
+#
+# this file is under public domain
+# caio begotti <caio1982@gmail.com>
 
-base_url='http://www.imdb.com/search/title?at=0&count=100&sort=moviemeter,asc&start=99951&title_type=feature&tok=17bd&year=1900,2015'
+_count() {
+	echo $(echo ${1} | sed 's/^.*start=\([0-9]\{1,\}\)\&.*/\1/g')
+}
 
-wget -x --load-cookies cookies.txt ${base_url} -O base_url.html
+_url() {
+	echo $(sed '/Next/!d;s/^.*="//g;s/".*$//g' ${1} | head -1)
+}
 
-url=$(sed '/Next/!d;s/^.*="//g;s/".*$//g' base_url.html | head -1)
+_cleanup() {
+	rm -vf .html *.html imdb.log imdb.search
+}
 
-while true; do
-	count=$(echo ${url} | sed 's/^.*start=\([0-9]\{1,\}\)\&.*/\1/g')
-	if [ ! -e ${count}.html ]; then
-		wget -x --load-cookies cookies.txt "http://www.imdb.com${url}" -O ${count}.html || exit 1
-	fi
-        url=$(sed '/Next/!d;s/^.*="//g;s/".*$//g' ${count}.html | head -1)
+_loop() {
+	while true; do
+		if [ ! -e "${count}.html" ]; then
+			# sometimes it just fails (either by some weird 500 error or just timeouts), so let's retry it
+			${mywget} "${imdb}${url}" -O ${count}.html || (rm ${count}.html; ${mywget} "${imdb}${url}" -O ${count}.html)
+		fi
+		url=$(_url ${count}.html)
+	        if [ "${url}" == "" ]; then export url='/search/title?at=0&count=100&sort=moviemeter,asc&start=99851&title_type=feature&tok=17bd&year=1900,2015'; _cleanup; break; break; fi
+		count=$(_count ${url})
+	        if [ "${count}" == "" ]; then export count='99851'; _cleanup; break; break; fi
+	done
+}
+
+mywget='wget -t 5 -T 30 -x --load-cookies cookies.txt'
+
+imdb=http://www.imdb.com
+startpage=99851
+limitpage=285000
+
+search="/search/title?at=0&count=100&sort=moviemeter,asc&start=${startpage}&title_type=feature&tok=17bd&year=1900,2015"
+fullsearch="${imdb}${search}"
+${mywget} ${fullsearch} -O imdb.search
+
+url="$(sed '/Next/!d;s/^.*="//g;s/".*$//g' imdb.search | head -1)"
+count="$(echo ${url} | sed 's/^.*start=\([0-9]\{1,\}\)\&.*/\1/g')"
+
+while [ "${count}" -lt "${limitpage}" ]; do
+	_loop
+	date >> imdb.log
 done
 
 exit 0
