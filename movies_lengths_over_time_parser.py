47415ff0101851c9b09522418f19ad7aff95f2f1
Caio Begotti <caio1982@gmail.com>
2014-04-02 21:16:24 -0300

    better error handling

--- movies_lengths_over_time_parser.py
+++ movies_lengths_over_time_parser.py
@@ -53,30 +53,39 @@ def appfunc():
                     rowdict['thumbnail'] = par.a.img['src']
 
                 # users votes and box office numbers
-                for par in parent('td', class_='sort_col'):
-                    boxre = RE_BOXOFFICE.findall(par.text)
-                    votesre = RE_VOTES.findall(par.text)
-                    if boxre:
-                        rowdict['boxoffice'] = boxre[0]
-                    elif votesre:
-                        rowdict['votes'] = votesre[0]
-                    else:
-                        rowdict['boxoffice'] = 'N/A'
-                        rowdict['votes'] = 'N/A'
+                sortcol = parent('td', class_='sort_col')
+                if not len(sortcol) == 0:
+                    for par in sortcol:
+                        boxre = RE_BOXOFFICE.findall(par.text)
+                        votesre = RE_VOTES.findall(par.text)
+                        if boxre:
+                            rowdict['boxoffice'] = boxre[0]
+                        elif votesre:
+                            rowdict['votes'] = votesre[0]
+                        else:
+                            rowdict['boxoffice'] = 'N/A'
+                            rowdict['votes'] = 'N/A'
+                else:
+                    rowdict['boxoffice'] = 'N/A'
+                    rowdict['votes'] = 'N/A'
 
                 # credits
                 credits = entry('span', class_='credit')
-                for credit in credits:
-                    director = RE_DIRECTOR.findall(credit.text)
-                    actors = RE_CAST.findall(credit.text)
-                    if director:
-                        rowdict['director'] = director[0]
-                    else:
-                        rowdict['director'] = 'N/A'
-                    if actors:
-                        rowdict['cast'] = actors[0]
-                    else:
-                        rowdict['cast'] = 'N/A'
+                if not len(credits) == 0:
+                    for credit in credits:
+                        director = RE_DIRECTOR.findall(credit.text)
+                        actors = RE_CAST.findall(credit.text)
+                        if director:
+                            rowdict['director'] = director[0]
+                        else:
+                            rowdict['director'] = 'N/A'
+                        if actors:
+                            rowdict['cast'] = actors[0]
+                        else:
+                            rowdict['cast'] = 'N/A'
+                else:
+                    rowdict['director'] = 'N/A'
+                    rowdict['cast'] = 'N/A'
 
                 # running time, in minutes
                 runtime = entry('span', class_='runtime')
@@ -84,6 +93,8 @@ def appfunc():
                     runtime_re = RE_RUNTIME.findall(runtime[0].text)
                     if runtime_re:
                         rowdict['runtime'] = runtime_re[0]
+                    else:
+                        rowdict['runtime'] = 'N/A'
                 else:
                     rowdict['runtime'] = 'N/A'
 
@@ -93,6 +104,8 @@ def appfunc():
                     year = RE_YEAR.findall(release[0].text)
                     if year:
                         rowdict['release'] = year[0]
+                    else:
+                        rowdict['release'] = 'N/A'
                 else:
                     rowdict['release'] = 'N/A'
 
@@ -106,20 +119,26 @@ def appfunc():
 
                 # certificate
                 certs = entry('span', class_='certificate')
-                for cert in certs:
-                    if cert.span:
-                        rowdict['certificate'] = cert.span['title']
-                    else:
-                        rowdict['certificate'] = 'N/A'
+                if not len(certs) == 0:
+                    for cert in certs:
+                        if cert.span:
+                            rowdict['certificate'] = cert.span['title']
+                        else:
+                            rowdict['certificate'] = 'N/A'
+                else:
+                    rowdict['certificate'] = 'N/A'
 
                 # rating
                 rates = entry('span', class_='value')
-                for rate in rates:
-                    rerate = RE_RATING.findall(rate.text)
-                    if rerate:
-                        rowdict['rating'] = rerate[0]
-                    else:
-                        rowdict['rating'] = 'N/A'
+                if not len(rates) == 0:
+                    for rate in rates:
+                        rerate = RE_RATING.findall(rate.text)
+                        if rerate:
+                            rowdict['rating'] = rerate[0]
+                        else:
+                            rowdict['rating'] = 'N/A'
+                else:
+                    rowdict['rating'] = 'N/A'
 
                 # for more information, will also be the key
                 link = 'http://www.imdb.com' + movie['href']
