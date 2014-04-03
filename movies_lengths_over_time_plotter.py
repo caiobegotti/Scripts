bc0568b5796a8f0361c6c44cba23667fbfa78068
Caio Begotti <caio1982@gmail.com>
2014-04-02 21:18:03 -0300

    testing something i could not remember but i need to pull back at the other place i cannot disclose...

--- movies_lengths_over_time_plotter.py
+++ movies_lengths_over_time_plotter.py
@@ -9,36 +9,49 @@ from matplotlib import pyplot
 import numpy
 import pandas
 
-columns = ['titles', 'years', 'runtimes', 'links']
-dtype = {'titles': numpy.object, 'years': numpy.int64, 'runtimes': numpy.int64, 'links': numpy.object}
-data = pandas.read_csv('imdb.txt', sep='\t', names=columns, dtype=dtype)
-years = list(data.years)
-runtimes = list(data.runtimes)
+columns = ['title', 'release', 'runtime', 'boxoffice', 'votes', 'rating', 'genre', 'certificate', 'director', 'cast', 'link', 'thumbnail']
+dtype = {'title': numpy.object, 'release': numpy.object, 'runtime': numpy.object, 'boxoffice': numpy.object, 'rating': numpy.object, 'genre': numpy.object, 'certificate': numpy.object, 'director': numpy.object, 'cast': numpy.object, 'link': numpy.object, 'thumbnail': numpy.object}
+data = pandas.read_csv('/tmp/imdb.csv', sep='\t', names=columns, dtype=dtype)
+allyears = list(data.release)
+years = []
+for y in allyears:
+    if y.isdigit():
+        years.append(y)
+    else:
+        years.append(2015)
+allruntimes = list(data.runtime)
+runtimes = []
+for r in allruntimes:
+    try:
+        if float(r):
+            runtimes.append(r)
+    except:
+        runtimes.append(9999)
 
 fig = pyplot.figure(figsize=(25, 25), dpi=100)
 
 # all movies plotted -------------------------
-#m = fig.add_subplot(211)
-#m.scatter(years, runtimes, marker='x', s=50, linewidths=0.25, alpha=0.25)
-#
+m = fig.add_subplot(211)
+m.scatter(years, runtimes, marker='x', s=50, linewidths=0.25, alpha=0.25)
+
 # we have only a few releases over 300 minutes
 # of length, they would pollute the plot
-#m.set_xlim([1900, 2015])
-#m.set_ylim([0, 300])
-#
+m.set_xlim([1900, 2015])
+m.set_ylim([0, 300])
+
 # averages per year --------------------------
-#averages = {}
-#for t in sorted(set(years)):
-#    thisyear = []
-#    for y, r in zip(years, runtimes):
-#        if y == t:
-#            thisyear.append(r)
-#    total = len(thisyear)
-#    averages[t] = sum(thisyear) / total
-#
-#a = fig.add_subplot(211)
-#a.plot(averages.keys(), averages.values(), color='w', linestyle='-', linewidth=5)
-#a.plot(averages.keys(), averages.values(), color='r', linestyle='-', linewidth=3)
+averages = {}
+for t in sorted(set(years)):
+    thisyear = []
+    for y, r in zip(years, runtimes):
+        if y == t:
+            thisyear.append(r)
+    total = len(thisyear)
+    averages[t] = sum(thisyear) / total
+
+a = fig.add_subplot(211)
+a.plot(averages.keys(), averages.values(), color='w', linestyle='-', linewidth=5)
+a.plot(averages.keys(), averages.values(), color='r', linestyle='-', linewidth=3)
 
 # total number of releases per year ----------
 #releases = {}
