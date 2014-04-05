6943a23ae3d40a9e0169fa0e21f1c50ddffe7b63
Caio Begotti <caio1982@gmail.com>
2014-04-05 18:36:26 -0300

    simplifying it, going back to basics to figure out a decent way to plot all that info in a meaningful way

--- movies_lengths_over_time_plotter.py
+++ movies_lengths_over_time_plotter.py
@@ -5,34 +5,34 @@
 # caio begotti <caio1982@gmail.com>
 
 from matplotlib import pyplot
+import csv
 
-import numpy
-import pandas
+def getColumn(filename, column):
+    results = csv.reader(open(filename), delimiter="|")
+    return [result[column] for result in results]
 
-columns = ['title', 'release', 'runtime', 'boxoffice', 'votes', 'rating', 'genre', 'certificate', 'director', 'cast', 'link', 'thumbnail']
-dtype = {'title': numpy.object, 'release': numpy.object, 'runtime': numpy.object, 'boxoffice': numpy.object, 'rating': numpy.object, 'genre': numpy.object, 'certificate': numpy.object, 'director': numpy.object, 'cast': numpy.object, 'link': numpy.object, 'thumbnail': numpy.object}
-data = pandas.read_csv('/tmp/imdb.csv', sep='\t', names=columns, dtype=dtype)
-allyears = list(data.release)
+year = getColumn("imdb.csv", 1)
+runtime = getColumn("imdb.csv", 3)
+
 years = []
-for y in allyears:
-    if y.isdigit():
+for y in year:
+    if not 'N/A' in y:
         years.append(y)
     else:
-        years.append(2015)
-allruntimes = list(data.runtime)
+        years.append('2015')
+
 runtimes = []
-for r in allruntimes:
-    try:
-        if float(r):
-            runtimes.append(r)
-    except:
-        runtimes.append(9999)
+for r in runtime:
+    if not 'N/A' in r:
+        runtimes.append(r)
+    else:
+        runtimes.append('0')
 
 fig = pyplot.figure(figsize=(25, 25), dpi=100)
 
 # all movies plotted -------------------------
 m = fig.add_subplot(211)
-m.scatter(years, runtimes, marker='x', s=50, linewidths=0.25, alpha=0.25)
+m.plot(years, runtimes, 'bx', alpha=0.25)
 
 # we have only a few releases over 300 minutes
 # of length, they would pollute the plot
@@ -40,19 +40,23 @@ m.set_xlim([1900, 2015])
 m.set_ylim([0, 300])
 
 # averages per year --------------------------
-averages = {}
-for t in sorted(set(years)):
-    thisyear = []
-    for y, r in zip(years, runtimes):
-        if y == t:
-            thisyear.append(r)
-    total = len(thisyear)
-    averages[t] = sum(thisyear) / total
-
-a = fig.add_subplot(211)
-a.plot(averages.keys(), averages.values(), color='w', linestyle='-', linewidth=5)
-a.plot(averages.keys(), averages.values(), color='r', linestyle='-', linewidth=3)
-
+#averages = {}
+#for t in sorted(set(years)):
+#    thisyear = []
+#    for y, r in zip(years, runtimes):
+#        if y == t:
+#            thisyear.append(r)
+#    total = len(thisyear)
+#    thisyear = [float(x) for x in thisyear]
+#    averages[t] = int(sum(thisyear) / total)
+#
+#for key in sorted(averages.iterkeys()):
+#    print key, averages[key]
+#
+#a = fig.add_subplot(211)
+#a.plot(averages.keys(), averages.values(), color='w', linestyle='-', linewidth=5)
+#a.plot(averages.keys(), averages.values(), color='r', linestyle='-', linewidth=3)
+#
 # total number of releases per year ----------
 #releases = {}
 #for t in sorted(set(years)):
@@ -65,7 +69,7 @@ a.plot(averages.keys(), averages.values(), color='r', linestyle='-', linewidth=3
 #r = fig.add_subplot(211)
 #r.set_xlim([1900, 2014])
 #r.plot(releases.keys(), releases.values(), color='g', linestyle='-', linewidth=5)
-
+#
 # stacked bars of common lengths -------------
 #stacked = {}
 #for t in sorted(set(years)):
@@ -90,13 +94,12 @@ a.plot(averages.keys(), averages.values(), color='r', linestyle='-', linewidth=3
 #    f.close()    
 
 # labels
-pyplot.title('Number of movies releases per year (~153k films)')
+pyplot.title('Movies releases and runtimes per year (~286k films)')
 pyplot.xlabel('Year of release')
-pyplot.ylabel('Total')
+pyplot.ylabel('Runtime in minutes')
 pyplot.grid(True)
 
 # increase x time resolution
-#yearsticks = sorted(set([int(x) for x in years if x % 10 == 0]))
-#pyplot.xticks(yearsticks)
-
+yearsticks = sorted(set([int(x) for x in years if int(x) % 5 == 0]))
+pyplot.xticks(yearsticks)
 pyplot.savefig('imdb_releases.png', bbox_inches='tight', dpi=100)
